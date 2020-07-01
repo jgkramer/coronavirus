7d3f7b3df65a86868c2bb443baed6c4c05429207
jgkramer <jared.kramer@gmail.com>
2020-07-01 15:06:26 -0400

    updates to pull RI data

--- get_data.R
+++ get_data.R
@@ -1,7 +1,22 @@
 
+library(googlesheets4)
+
 if(!file.exists("./downloads"))
   dir.create("./downloads")
 
 url_ct <- "https://data.ct.gov/api/views/rf3k-f8fg/rows.csv?accessType=DOWNLOAD"
 csv_ct <- "./downloads/ct.csv"
 download.file(url = url_ct, destfile = csv_ct)
+
+url_ri <- "https://docs.google.com/spreadsheets/d/1n-zMS9Al94CPj_Tc3K7Adin-tN9x1RSjjx2UzJ4SV7Q/"
+csv_ri <- "./downloads/ri.csv"
+RI <- read_sheet(url_ri, sheet = "COVID Trends")
+RI$ICU[RI$ICU == "NULL"] <- NA
+RI$ICU <- as.numeric(unlist(RI$ICU))
+
+RI$Vented[RI$Vented == "NULL"] <- NA
+RI$Vented <- as.numeric(unlist(RI$Vented))
+
+RI <- arrange(RI, desc(Date))
+
+write.csv(RI, file = csv_ri, row.names = TRUE)
--- read_data.R
+++ read_data.R
@@ -11,8 +11,10 @@ LEN = dim(df_ct)[1]
 
 New.Tests = df_ct[1:LEN-1, "Tests"] - df_ct[2:LEN, "Tests"]
 New.Cases = df_ct[1:LEN-1, "Cases"] - df_ct[2:LEN, "Cases"]
+New.Hosp = df_ct[1:LEN-1, "Hospitalized"] - df_ct[2:LEN, "Hospitalized"]
 Cases.Growth = New.Cases / (df_ct[2:LEN, "Cases"])
 
-df_changes_ct = data.frame(Date = df_ct[1:LEN-1, "Date"], New.Tests, New.Cases, Positive = New.Cases/New.Tests, Cases.Growth)
+df_changes_ct = data.frame(Date = df_ct[1:LEN-1, "Date"], New.Tests, New.Cases, New.Hosp, Positive = New.Cases/New.Tests, Cases.Growth)
 
-print(df_changes_ct)
+print(head(df_changes_ct, 20))
+plot(x = df_changes_ct[, "Date"], y = df_changes_ct[, "New.Cases"])

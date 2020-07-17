ce3ca16a68143c0442960de391d54a74cb975b3e
jgkramer <jared.kramer@gmail.com>
2020-07-17 00:02:11 -0400

    Minor updates

--- get_data.R
+++ get_data.R
@@ -1,13 +1,21 @@
 
 library(googlesheets4)
+library(stringr)
+library(dplyr)
 
 if(!file.exists("./downloads"))
   dir.create("./downloads")
 
+if(!file.exists("./downloads/mass"))
+  dir.create("./downloads/mass")
+
+#CONNECTICUT
 url_ct <- "https://data.ct.gov/api/views/rf3k-f8fg/rows.csv?accessType=DOWNLOAD"
 csv_ct <- "./downloads/ct.csv"
 download.file(url = url_ct, destfile = csv_ct)
 
+
+#RHODE ISLAND
 url_ri <- "https://docs.google.com/spreadsheets/d/1n-zMS9Al94CPj_Tc3K7Adin-tN9x1RSjjx2UzJ4SV7Q/"
 csv_ri <- "./downloads/ri.csv"
 RI <- read_sheet(url_ri, sheet = "COVID Trends")
@@ -16,7 +24,17 @@ RI$ICU <- as.numeric(unlist(RI$ICU))
 
 RI$Vented[RI$Vented == "NULL"] <- NA
 RI$Vented <- as.numeric(unlist(RI$Vented))
-
 RI <- arrange(RI, desc(Date))
 
 write.csv(RI, file = csv_ri, row.names = TRUE)
+
+#MASS
+url_ma_scrape <- "https://www.mass.gov/info-details/covid-19-response-reporting"
+landingPage <- readLines(url_ma_scrape)
+matches <- str_match(landingPage, "/doc/covid-19-raw-data-.*/download")
+url_ma_data <- paste("https://www.mass.gov", matches[!is.na(matches)], sep="")
+
+
+today_dir <- paste("./downloads/mass/", Sys.Date(), sep="")
+download.file(url_ma_data, "./downloads/mass/mass.zip", mode="wb")
+unzip("./downloads/mass/mass.zip", exdir = today_dir)
--- read_data.R
+++ read_data.R
@@ -4,17 +4,24 @@ csv_ct <- "./downloads/ct.csv"
 df_ct <- read.csv(csv_ct)
 
 df_ct <- df_ct %>%
+  mutate(Date = as.Date(as.character(Date), format = "%m/%d/%Y")) %>%
   arrange(desc(Date)) %>% 
   rename(Tests = COVID.19.PCR.tests.reported, Cases = Total.cases, Hospitalized = Hospitalized.cases, Deaths = Total.deaths) %>%
-  select(c(Date, Tests, Cases, Hospitalized, Deaths)) 
+  select(c(Date, Tests, Cases, Hospitalized, Deaths))
 LEN = dim(df_ct)[1]
 
-New.Tests = df_ct[1:LEN-1, "Tests"] - df_ct[2:LEN, "Tests"]
-New.Cases = df_ct[1:LEN-1, "Cases"] - df_ct[2:LEN, "Cases"]
+nDays = as.numeric(df_ct[1:LEN-1, "Date"] - df_ct[2:LEN, "Date"])
+New.Tests = (df_ct[1:LEN-1, "Tests"] - df_ct[2:LEN, "Tests"])
+New.Cases = (df_ct[1:LEN-1, "Cases"] - df_ct[2:LEN, "Cases"])
 New.Hosp = df_ct[1:LEN-1, "Hospitalized"] - df_ct[2:LEN, "Hospitalized"]
 Cases.Growth = New.Cases / (df_ct[2:LEN, "Cases"])
 
-df_changes_ct = data.frame(Date = df_ct[1:LEN-1, "Date"], New.Tests, New.Cases, New.Hosp, Positive = New.Cases/New.Tests, Cases.Growth)
+df_changes_ct = data.frame(Date = df_ct[1:LEN-1, "Date"],
+                           nDays = nDays,
+                          New.Tests, 
+                          New.Cases, 
+                          New.Hosp, 
+                          Positive = New.Cases/New.Tests, Cases.Growth)
 
 print(head(df_changes_ct, 20))
 plot(x = df_changes_ct[, "Date"], y = df_changes_ct[, "New.Cases"])
